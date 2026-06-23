e49e1c6a1bffd935a0b64109ce2f8e92add0b500
Ernest Zolotarev <1ErnestZolotarev@users.noreply.github.com>
2026-06-23 07:58:11 +0200

    Add missing import for debugPrint

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user_model.dart';
@@ -8,28 +9,23 @@ class AuthService {
 
   Stream<User?> get authStateChanges => _auth.authStateChanges();
 
-  /// Вход/регистрация по номеру телефона
   Future<AppUser?> signInWithPhone({
     required String name,
     required String phone,
     required String city,
     required String role,
   }) async {
-    // Сначала аутентифицируемся анонимно
     final credential = await _auth.signInAnonymously();
     final user = credential.user;
     if (user == null) return null;
 
-    // Проверяем, существует ли пользователь с таким телефоном
     final existingUser = await _getUserByPhone(phone);
     
     if (existingUser != null) {
-      // Проверяем, не забанен ли
       if (existingUser.isBanned) {
         throw Exception('Аккаунт заблокирован: ${existingUser.bannedReason ?? "Причина не указана"}');
       }
       
-      // Обновляем данные существующего пользователя
       final updatedUser = AppUser(
         uid: user.uid,
         name: name,
@@ -47,7 +43,6 @@ class AuthService {
       await _firestore.collection('users').doc(user.uid).set(updatedUser.toMap());
       return updatedUser;
     } else {
-      // Создаём нового пользователя
       final appUser = AppUser(
         uid: user.uid,
         name: name,
