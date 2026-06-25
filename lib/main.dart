b5ae54b2dec59799b77a65874045b3644854ee65
Ernest Zolotarev <1ErnestZolotarev@users.noreply.github.com>
2026-06-25 14:22:14 +0200

    Remove online timer to fix Provider error

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'firebase_options.dart';
 import 'providers/auth_provider.dart' as OurAuth;
 import 'providers/order_provider.dart';
@@ -20,48 +18,19 @@ void main() async {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Timer? _onlineTimer;
-  bool _timerStarted = false;
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    if (!_timerStarted) {
-      _startOnlineTimer();
-      _timerStarted = true;
-    }
-  }
-
-  void _startOnlineTimer() {
-    _onlineTimer = Timer.periodic(const Duration(seconds: 30), (_) {
-      final user = context.read<OurAuth.AuthProvider>().user;
-      if (user != null && user.phone.isNotEmpty) {
-        final docId = user.phone.replaceAll(RegExp(r'\D'), '');
-        FirebaseFirestore.instance.collection('users').doc(docId).update({
-          'lastSeen': DateTime.now().toIso8601String(),
-        });
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _onlineTimer?.cancel();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) => MultiProvider(
-    providers: [ChangeNotifierProvider(create: (_) => OurAuth.AuthProvider()), ChangeNotifierProvider(create: (_) => OrderProvider())],
+    providers: [
+      ChangeNotifierProvider(create: (_) => OurAuth.AuthProvider()),
+      ChangeNotifierProvider(create: (_) => OrderProvider()),
+    ],
     child: MaterialApp(
-      title: 'ААСтройПро', theme: ThemeData(primarySwatch: Colors.orange), debugShowCheckedModeBanner: false,
+      title: 'ААСтройПро',
+      theme: ThemeData(primarySwatch: Colors.orange),
+      debugShowCheckedModeBanner: false,
       home: Consumer<OurAuth.AuthProvider>(builder: (_, a, __) => a.user != null
         ? AdaptiveLayout(mobileBody: const HomeScreen(), tabletBody: const Row(children: [Expanded(flex:2, child: HomeScreen()), VerticalDivider(width:1), Expanded(flex:3, child: Center(child: Text('Выберите объявление')))]))
         : const StartScreen()),
