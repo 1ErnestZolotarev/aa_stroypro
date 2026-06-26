f5dd89be74d36ea6e3653e53d8355eeeaed6a368
Ernest Zolotarev <1ErnestZolotarev@users.noreply.github.com>
2026-06-26 16:33:27 +0200

    Fix groovy-xml dependency and clean Gradle cache

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.google.gms:google-services:4.4.2")
-        classpath("org.codehaus.groovy:groovy-xml:3.0.9")   // <-- добавлено
+        classpath("org.codehaus.groovy:groovy-xml:3.0.9")   // <-- обязательно
     }
 }
 
