f7ff71b56e09d1921019077a7a7afc10cfa60aca
Ernest Zolotarev <1ErnestZolotarev@users.noreply.github.com>
2026-06-27 23:06:04 +0200

    fix: исправлен синтаксис firebase_options.dart

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -6,5 +6,5 @@ const FirebaseOptions firebaseOptions = FirebaseOptions(
   projectId: 'aa-stroypro-apk-v2-9b801',
   storageBucket: 'aa-stroypro-apk-v2-9b801.firebasestorage.app',
   appId: '1:255181738766:web:f3e3cbe9ac4e82a84e3d5f',
-  messagingSenderId: '255181738766',   // обязательное поле
+  messagingSenderId: '255181738766',
 );
