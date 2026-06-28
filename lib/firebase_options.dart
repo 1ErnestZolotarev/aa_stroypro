568e7e66439b1aad9d153855dd582a463d93f43d
Ernest Zolotarev <1ErnestZolotarev@users.noreply.github.com>
2026-06-28 07:46:53 +0200

    Fix Dart syntax and add groovy-xml dependency

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,22 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import 'package:firebase_core/firebase_core.dart';
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyD1Cahy8-hRlmafbnFiodukEjwIgyOa7Gc",
-  authDomain: "aa-stroypro-apk-v2-9b801.firebaseapp.com",
-  databaseURL: "https://aa-stroypro-apk-v2-9b801-default-rtdb.firebaseio.com",
-  projectId: "aa-stroypro-apk-v2-9b801",
-  storageBucket: "aa-stroypro-apk-v2-9b801.firebasestorage.app",
-  messagingSenderId: "255181738766",
-  appId: "1:255181738766:web:f3e3cbe9ac4e82a84e3d5f",
-  measurementId: "G-7261VMHYP2"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyD1Cahy8-hRlmafbnFiodukEjwIgyOa7Gc",
+      authDomain: "aa-stroypro-apk-v2-9b801.firebaseapp.com",
+      projectId: "aa-stroypro-apk-v2-9b801",
+      storageBucket: "aa-stroypro-apk-v2-9b801.firebasestorage.app",
+      messagingSenderId: "255181738766",
+      appId: "1:255181738766:web:f3e3cbe9ac4e82a84e3d5f",
+      measurementId: "G-7261VMHYP2",
+    );
+  }
+}
