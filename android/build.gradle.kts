e0260ce7b03c8195fab1d95c67cd332b91849093
Ernest Zolotarev <1ErnestZolotarev@users.noreply.github.com>
2026-07-01 07:58:37 +0200

    Restore default Gradle build paths

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -7,7 +7,6 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.7.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
         classpath("com.google.gms:google-services:4.4.2")
-        classpath("org.apache.groovy:groovy-xml:4.0.0")
     }
 }
 
