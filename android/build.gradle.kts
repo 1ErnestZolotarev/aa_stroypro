e42147f7510f5e4056906c49ac3a10e86e374ce4
Ernest Zolotarev <1ErnestZolotarev@users.noreply.github.com>
2026-07-01 09:25:43 +0200

    Upgrade AGP and Kotlin for R8 compatibility

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.7.0")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.android.tools.build:gradle:8.10.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
